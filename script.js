59da771db338a8efa211d60e03b2199155945b5e
Tanvish jha <Tanvish@users.noreply.github.com>
2017-11-26 12:35:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,159 @@
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 180 && $(document).scrollTop()<2200) {
+      $('.planet').addClass('planet-move');
+    }
+   
+    else {
+    $('.planet').removeClass('planet-move');
+    }
+  });
+});
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop()>180) {
+      $('.logo').addClass('logo-hide');
+    }
+   
+    else {
+    $('.logo').removeClass('logo-hide');
+    }
+  });
+});
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop()>180) {
+      $('.logo-side').addClass('logo-hide');
+    }
+   
+    else {
+    $('.logo-side').removeClass('logo-hide');
+    }
+  });
+});
+
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 2100) {
+      $('.planet').addClass('planet-remove');
+    }
+   
+    else {
+    $('.planet').removeClass('planet-remove');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 470 && $(document).scrollTop() < 800) {
+      $('.feature-1').addClass('feature-1-show');
+    }
+   
+    else {
+    $('.feature-1').removeClass('feature-1-show');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 470 && $(document).scrollTop() < 800) {
+      $('.image-1').addClass('image-1-show');
+    }
+   
+    else {
+    $('.image-1').removeClass('image-1-show');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 470 && $(document).scrollTop() < 800) {
+      $('.image-2').addClass('image-2-show');
+    }
+   
+    else {
+    $('.image-2').removeClass('image-2-show');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 470 && $(document).scrollTop() < 800) {
+      $('.image-3').addClass('image-3-show');
+    }
+   
+    else {
+    $('.image-3').removeClass('image-3-show');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 900 && $(document).scrollTop() < 1300) {
+      $('.feature-2').addClass('feature-2-show');
+    }
+   
+    else {
+    $('.feature-2').removeClass('feature-2-show');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 900 && $(document).scrollTop() < 1300) {
+      $('.image-4').addClass('image-4-show');
+    }
+   
+    else {
+    $('.image-4').removeClass('image-4-show');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 900 && $(document).scrollTop() < 1300) {
+      $('.image-5').addClass('image-5-show');
+    }
+   
+    else {
+    $('.image-5').removeClass('image-5-show');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 1500 && $(document).scrollTop() < 1900) {
+      $('.image-6').addClass('image-6-show');
+    }
+   
+    else {
+    $('.image-6').removeClass('image-6-show');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 1500 && $(document).scrollTop() < 1900) {
+      $('.feature-3').addClass('feature-1-show');
+    }
+   
+    else {
+    $('.feature-3').removeClass('feature-1-show');
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 2100 && $(document).scrollTop() < 2400) {
+      $('.feature-4').addClass('feature-2-show');
+    }
+   
+    else {
+    $('.feature-4').removeClass('feature-2-show');
+    }
+  });
+});
